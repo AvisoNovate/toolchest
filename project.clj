a9e5ae30951d5ca12c9faa0a8ee75487333c6ae2
Howard M. Lewis Ship <hlship@gmail.com>
2014-11-17 15:20:15 -0800

    Advance version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.aviso/toolchest "0.1.0"
+(defproject io.aviso/toolchest "0.1.1-SNAPSHOT"
             :description "Generally useful macros and functions"
             :url "https://github.com/AvisoNovate/toolchest"
             :license {:name "Apache Sofware License 2.0"
